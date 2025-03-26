21db2d02165b503f6b733e0c0faada50c927bdf5
Шаров Глеб <glscharow@yandex.ru>
2025-03-26 13:01:05 +0700

    Fix code

--- src/data_science/processing_data.py
+++ src/data_science/processing_data.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
-import re
 
 df = pd.read_csv("../utils/hata.csv")
+
 
 def clean_data(data_frame):
     print("\nHandling Missing Data...")
@@ -13,29 +13,26 @@ def clean_data(data_frame):
     print(f"Cleaned Data: {len(data_frame)} rows remaining.")
     return data_frame
 
-df = clean_data(df)
 
-def extract_numeric(value):
-    if isinstance(value, str):
-        match = re.search(r'\d+', value)  # Find first numeric value
-        return float(match.group()) if match else np.nan
-    return np.nan
+df = clean_data(df)
 
 
 def convert_columns(df):
     print("Converting Columns...")
-    df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
     df['Price'] = df['Price'].str.replace('[$,₽/мес. ]', '', regex=True).replace('', np.nan).astype(float)
     print("Data Cleaning Complete!")
     return df
+
 
 df = convert_columns(df)
 
 
 def show_extreme_listings(df):
+    # Выводить топ 5 самых дорогих аренд и топ 5 самых дешевых
     print("\nTop 5 Most Expensive Listings:")
     print(df.nlargest(5, 'Price')["Price"])
     print("\nTop 5 Cheapest Listings:")
     print(df.nsmallest(5, 'Price')["Price"])
+
 
 show_extreme_listings(df)
